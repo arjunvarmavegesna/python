5a55f591138921028847cc9ec3896a92297ed491
arjunvarmavegesna <arjunvarmavegesna@gmail.com>
2025-08-20 18:08:03 +0530

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,4 @@
+print("WELCOME TO BAND NAME GENERATOR!")
+city = input("which city did you grow up in?\n")
+pet = input("what's the name of your pet?\n")
+print("Your band name could be " + city + " " + pet)
---
+++ print_statement_practice.py
@@ -0,0 +1,4 @@
+print("Day 1 - String Manupulation")
+print("string concatenation is done with the '+' sign.")
+print('e.g. print("Hello "+"World")')
+print("New Lines can be created with a backslash and n. ")
